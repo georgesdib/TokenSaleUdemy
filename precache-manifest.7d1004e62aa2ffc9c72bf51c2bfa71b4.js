f5831cf36a2650615ca8a27b479da3ad6ba6f015
Georges Dib <georges.dib@gmail.com>
2021-06-05 08:30:42 +0100

    Updates

---
+++ precache-manifest.7d1004e62aa2ffc9c72bf51c2bfa71b4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79e88f41ca6574c688428b82133f5397",
+    "url": "/TokenSaleUdemy/index.html"
+  },
+  {
+    "revision": "26c91ec577a1c7df7fae",
+    "url": "/TokenSaleUdemy/static/css/main.a3487775.chunk.css"
+  },
+  {
+    "revision": "4daf67644f64743a4985",
+    "url": "/TokenSaleUdemy/static/js/2.8400b2fc.chunk.js"
+  },
+  {
+    "revision": "26c91ec577a1c7df7fae",
+    "url": "/TokenSaleUdemy/static/js/main.652bf84a.chunk.js"
+  },
+  {
+    "revision": "c9704d3e9efdc7c88e54",
+    "url": "/TokenSaleUdemy/static/js/runtime-main.f89531fb.js"
+  }
+]);
